c90cb303cf2f719b3aa5c1dcdf20e9089f05a2c5
Anupam <puspaanupam@gmail.com>
2022-11-04 20:31:13 +0545

    drpdown

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import Openingsoon from '../pop_over/Openingsoon'
 import {useState} from 'react'
+import {RiArrowDropDownLine} from 'react-icons/ri'
 const Booking = () => {
   const [visible,setVisible]=useState(false)
   const close_visbile=()=>setVisible(false)
+  const [drop,setDrop]=useState(false)
+  const [dropword,setDropword]=useState("Type of Property")
+
+  const setHotelType=()=>{
+    setDropword('Hotel')
+    setDrop(false)
+  }
+  const setHostelType=()=>{
+    setDropword('Hostel')
+    setDrop(false)
+  }
+  const setResortType=()=>{
+    setDropword('Resort')
+    setDrop(false)
+  }
+
   return (
     <div className="booking  text-white">
         <div>
@@ -48,6 +65,12 @@ const Booking = () => {
           </div>
         </div>
       </div>
+
+
+
+      
+
+
       <div className="mb-5">
         <label
           for="guest"
@@ -64,6 +87,24 @@ const Booking = () => {
           className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
         />
       </div>
+
+
+    <div className="my-10">
+      <div className="bg-[rgb(34,45,63)] p-3 rounded-[5px] flex gap-1 items-center justify-center w-[300px] cursor-pointer hover:scale-110 hover:bg-[rgb(34,45,63)] duration-300 ease-in-out" onClick={()=>setDrop(!drop)}>
+
+      <div className="font-medium "  >{dropword} </div>
+      <div className=""><RiArrowDropDownLine size={60}/></div> 
+      </div>
+
+{drop ?  <ul className="bg-[rgb(34,45,63)] p-3 mt-3 rounded-xl">
+        <li className='hover:bg-[rgb(22,33,50)] mt-2 cursor-pointer p-2 ' onClick={setHotelType}>Hotel</li>
+        <li className='hover:bg-[rgb(22,33,50)] mt-2 cursor-pointer p-2 ' onClick={setHostelType}>Hostel</li>
+        <li className='hover:bg-[rgb(22,33,50)] mt-2 cursor-pointer p-2 ' onClick={setResortType}>Resort</li>
+      </ul>: ""}
+     
+    </div>
+
+
 
       <div className="-mx-3 flex flex-wrap">
         <div className="w-full px-3 sm:w-1/2">
